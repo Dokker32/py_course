cf8143d3dd6cf22fc9a9b8c145450c945dd48c37
Dmitriy <dokker_32@mail.ru>
2023-09-09 00:54:37 +0700

    Correct oop.py

---
+++ oop.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+person1 = Person("Иван", 30)
+person2 = Person("Мария", 25)
+
+print(person1.name)
+print(person2.age)
